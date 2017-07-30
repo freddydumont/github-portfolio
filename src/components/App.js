3ff99e6a93c61addb6f01aeaa3d39232e7592a58
freddydumont <frederickmorin18@gmail.com>
2017-07-30 19:07:06 -0400

    Add sidebar

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 import '../styles/App.css';
+import SideContent from './SideContent';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="App container">
+        <div className="row">
+          <SideContent />
+        </div>
       </div>
     );
   }
