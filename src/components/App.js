5f0afcce319ab0d6e1774e8e1e76a0ef3ad04b36
freddydumont <frederickmorin18@gmail.com>
2017-07-31 13:44:37 -0400

    Create project cards with a function

--- src/components/App.js
+++ src/components/App.js
@@ -6,15 +6,24 @@ import ProjectCard from './ProjectCard';
 
 class App extends Component {
   render() {
+    const createCards = () => {
+      let cards = [];
+      for (var i = 0; i < 4; i++) {
+        cards.push(
+          <ProjectCard projectImage={projectImages[i]} />
+        );
+      }
+      return cards;
+    }
+
     return (
       <div className="App container">
         <div className="row">
-          <aside className="col-sm-3 about-me about-side text-center">
           <aside className="col-sm-3 about-me text-center">
             <SideContent />
           </aside>
           <section className="col-sm-9">
-            <ProjectCard projectImage={projectImages[0]} />
+            {createCards()}
           </section>
         </div>
       </div>
