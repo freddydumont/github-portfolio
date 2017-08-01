357f003cddf39d50dfdaae3499f39dc462a11ba0
freddydumont <frederickmorin18@gmail.com>
2017-07-31 23:48:39 -0400

    Move image width management from jsx to css

--- src/cardData.js
+++ src/cardData.js
@@ -21,9 +21,9 @@ export default [
     description: ['A port of the classic electronic game ', <a href="https://en.wikipedia.org/wiki/Simon_(game)" title="Wikipedia page of Simon game" target="_blank" rel="noopener noreferrer">Simon</a>, ' with a Pokémon theme showcasing usage of Redux asynchronous actions.'],
     technology: "Poké(Si)mon is a React application making use of Redux thunks to generate the game loop. Flexbox is used for the main layout and CSS Grid for the Pokémon buttons. The sound is managed by the powerful Howler library. Most icons were manipulated in Adobe Illustrator to achieve the desired result.",
     techLogos: [
-      <img src={howler} alt="Howler" className="logo" width="100" />,
-      <img src={react} alt="React" className="logo" width="100" />,
-      <img src={redux} alt="Redux" className="logo" width="100" />,
+      <img src={howler} alt="Howler" className="logo" />,
+      <img src={react} alt="React" className="logo" />,
+      <img src={redux} alt="Redux" className="logo" />,
     ]
   },
   {
@@ -33,11 +33,11 @@ export default [
     description: "A Single Page Application demonstrating routing in a React & Redux environment. What started as a simple tic tac toe game ended up being a fairly big project using plenty of different tools.",
     technology: "Minimax's is a React Redux application using React Router to manage the multiple routes. Animations are executed with jQuery and Typed.js is used for the text. The tic tac toe algorithm comes from an API and requests are handled with Axios and Redux Promise.",
     techLogos: [
-      <img src={typed} alt="Typed.js" className="logo" width="100" />,
-      <img src={react} alt="React" className="logo" width="100" />,
-      <img src={redux} alt="Redux" className="logo" width="100" />,
-      // <img src={router} alt="React Router" className="logo" width="100" />,
-      <img src={jquery} alt="jQuery" className="logo" width="100" />,
+      <img src={typed} alt="Typed.js" className="logo" />,
+      <img src={react} alt="React" className="logo" />,
+      <img src={redux} alt="Redux" className="logo" />,
+      <img src={jquery} alt="jQuery" className="logo" />,
+      // <img src={router} alt="React Router" className="logo" />,
     ]
   },
 ]
