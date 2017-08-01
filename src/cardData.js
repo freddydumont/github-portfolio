c0d35f39deb4a428142ad8bc19108ce72b5872a5
freddydumont <frederickmorin18@gmail.com>
2017-08-01 14:38:39 -0400

    Add keys to each elements in arrays as per React requirements

--- src/cardData.js
+++ src/cardData.js
@@ -19,12 +19,12 @@ export default [
     projectImage: pokesimon,
     link: "https://freddydumont.github.io/simon-game/",
     name: "Poké(Si)mon",
-    description: ['A port of the classic electronic game ', <a href="https://en.wikipedia.org/wiki/Simon_(game)" title="Wikipedia page of Simon game" target="_blank" rel="noopener noreferrer">Simon</a>, ' with a Pokémon theme showcasing usage of Redux asynchronous actions.'],
+    description: ['A port of the classic electronic game ', <a key={'simon wiki'} href="https://en.wikipedia.org/wiki/Simon_(game)" title="Wikipedia page of Simon game" target="_blank" rel="noopener noreferrer">Simon</a>, ' with a Pokémon theme showcasing usage of Redux asynchronous actions.'],
     technology: "Poké(Si)mon is a React application making use of Redux thunks to generate the game loop. Flexbox is used for the main layout and CSS Grid for the Pokémon buttons. The sound is managed by the powerful Howler library. Most icons were manipulated in Adobe Illustrator to achieve the desired result.",
     techLogos: [
-      <img src={howler} alt="Howler" className="logo" />,
-      <img src={react} alt="React" className="logo" />,
-      <img src={redux} alt="Redux" className="logo" />,
+      <img src={howler} alt="Howler" key="Howler" className="logo" />,
+      <img src={react} alt="React" key="React" className="logo" />,
+      <img src={redux} alt="Redux" key="Redux" className="logo" />,
     ]
   },
   {
@@ -34,11 +34,10 @@ export default [
     description: "A Single Page Application demonstrating routing in a React & Redux environment. What started as a simple tic tac toe game ended up being a fairly big project using plenty of different tools.",
     technology: "Minimax's is a React Redux application using React Router to manage the multiple routes. Animations are executed with jQuery and Typed.js is used for the text. The tic tac toe algorithm comes from an API and requests are handled with Axios and Redux Promise.",
     techLogos: [
-      <img src={typed} alt="Typed.js" className="logo" />,
-      <img src={react} alt="React" className="logo" />,
-      <img src={redux} alt="Redux" className="logo" />,
-      <img src={jquery} alt="jQuery" className="logo" />,
-      // <img src={router} alt="React Router" className="logo" />,
+      <img src={typed} alt="Typed.js" key="Typed.js" className="logo" />,
+      <img src={react} alt="React" key="React" className="logo" />,
+      <img src={redux} alt="Redux" key="Redux" className="logo" />,
+      <img src={jquery} alt="jQuery" key="jQuery" className="logo" />,
     ]
   },
   {
@@ -48,8 +47,8 @@ export default [
     description: ["A time management tool based on the ", <a key={'pomodoro wiki'} href="https://en.wikipedia.org/wiki/Pomodoro_Technique" target="_blank" rel="noopener noreferrer">Pomodoro Technique</a>, " allowing the user to set work intervals separated by short breaks. A soothing burmese bell sound annouces the end of a session."],
     technology: "Pomodoro is a React application using Bootstrap for the layout and Moment.js to format the timer.",
     techLogos: [
-      <img src={react} alt="React" className="logo" />,
-      <img src={bootstrap} alt="Bootstrap" className="logo" />,
+      <img src={react} alt="React" key="React" className="logo" />,
+      <img src={bootstrap} alt="Bootstrap" key="Bootstrap" className="logo" />,
     ]
   },
 ]
