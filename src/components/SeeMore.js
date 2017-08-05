2d75ddd661dfc2b2f774277a6e2befd302b3d295
freddydumont <frederickmorin18@gmail.com>
2017-08-05 12:57:23 -0400

    Add non breaking space between github text and icon
    For small screens

--- src/components/SeeMore.js
+++ src/components/SeeMore.js
@@ -3,7 +3,9 @@ import React from 'react';
 const SeeMore = () => (
   <div className="col-sm-offset-2 col-sm-8 text-center" style={{ marginBottom: '100px' }}>
     <h3>
-      See more on <a href="https://github.com/freddydumont" title="GitHub Page">Github <i className="fa fa-github"></i></a>
+      See more on <a href="https://github.com/freddydumont" title="GitHub Page">
+        <span style={{ whiteSpace: 'nowrap', lineHeight: '1.5' }}>Github <i className="fa fa-github"></i></span>
+      </a>
     </h3>
   </div>
 );
