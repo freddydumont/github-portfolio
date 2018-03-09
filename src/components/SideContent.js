95ceeffb95b5ce86bff8280f8bc1ab933be6285e
Unknown <frederickmorin18@gmail.com>
2018-03-09 22:51:14 +1300

    Add social links

--- src/components/SideContent.js
+++ src/components/SideContent.js
@@ -9,6 +9,10 @@ const SideContent = () => (
 
     <div style={{ margin: '20px' }} className="text-center">
       <a href="https://github.com/freddydumont" title="GitHub Page" className="icon-contact"><i className="fa fa-github"></i></a>
+      <a href="https://twitter.com/_freddydumont" title="Twitter Page" className="icon-contact"><i className="fa fa-twitter"></i></a>
+      <a href="https://linkedin.com/in/freddydumont" title="LinkedIn Page" className="icon-contact"><i className="fa fa-linkedin-square"></i></a>
+      <a href="https://medium.com/@freddydumont" title="Medium Page" className="icon-contact"><i className="fa fa-medium"></i></a>
+      <a href="https://www.recursivewandering.com" title="Blog" className="icon-contact"><i className="fa fa-globe"></i></a>
       <a href="mailto:frederickmorin18@gmail.com" title="Email link" className="icon-contact"><i className="fa fa-envelope"></i></a>
     </div>
   </div>
