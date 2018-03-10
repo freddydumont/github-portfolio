1a40d802491b54f899de24cfd3e78af94a42712c
Unknown <frederickmorin18@gmail.com>
2018-03-10 16:40:28 +1300

    Update to fontawesome 5

--- src/components/SeeMore.js
+++ src/components/SeeMore.js
@@ -4,7 +4,7 @@ const SeeMore = () => (
   <div className="col-sm-offset-2 col-sm-8 text-center" style={{ marginBottom: '100px' }}>
     <h3>
       See more on <a href="https://github.com/freddydumont" title="GitHub Page">
-        <span style={{ whiteSpace: 'nowrap', lineHeight: '1.5' }}>Github <i className="fa fa-github"></i></span>
+        <span style={{ whiteSpace: 'nowrap', lineHeight: '1.5' }}>Github <i className="fab fa-github"></i></span>
       </a>
     </h3>
   </div>
--- src/components/SideContent.js
+++ src/components/SideContent.js
@@ -8,12 +8,12 @@ const SideContent = () => (
     <h2>Front-End Developer</h2>
 
     <div style={{ margin: '20px' }} className="text-center">
-      <a href="https://github.com/freddydumont" title="GitHub Page" className="icon-contact"><i className="fa fa-github"></i></a>
-      <a href="https://twitter.com/_freddydumont" title="Twitter Page" className="icon-contact"><i className="fa fa-twitter"></i></a>
-      <a href="https://linkedin.com/in/freddydumont" title="LinkedIn Page" className="icon-contact"><i className="fa fa-linkedin-square"></i></a>
-      <a href="https://medium.com/@freddydumont" title="Medium Page" className="icon-contact"><i className="fa fa-medium"></i></a>
+      <a href="https://github.com/freddydumont" title="GitHub Page" className="icon-contact"><i className="fab fa-github-square"></i></a>
+      <a href="https://twitter.com/_freddydumont" title="Twitter Page" className="icon-contact"><i className="fab fa-twitter-square"></i></a>
+      <a href="https://linkedin.com/in/freddydumont" title="LinkedIn Page" className="icon-contact"><i className="fab fa-linkedin"></i></a>
+      <a href="https://medium.com/@freddydumont" title="Medium Page" className="icon-contact"><i className="fab fa-medium"></i></a>
+      <a href="mailto:frederickmorin18@gmail.com" title="Email link" className="icon-contact"><i className="fa fa-envelope-square"></i></a>
       <a href="https://www.recursivewandering.com" title="Blog" className="icon-contact"><i className="fa fa-globe"></i></a>
-      <a href="mailto:frederickmorin18@gmail.com" title="Email link" className="icon-contact"><i className="fa fa-envelope"></i></a>
     </div>
   </div>
 );
